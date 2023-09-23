81eeaff8678b7668533588bb1b03d3b060f78ff2
Nickolai Grachev <nickbd95@gmail.com>
2023-09-23 20:24:06 +0300

    first commit

--- traficLight/ViewController.swift
+++ traficLight/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var traficLight: UIStackView!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        traficLight.viewWithTag(1)?.alpha = 0.3
+        traficLight.viewWithTag(2)?.alpha = 0.3
+        traficLight.viewWithTag(3)?.alpha = 0.3
+        
+        traficLight.viewWithTag(1)?.layer.cornerRadius = 90
+        traficLight.viewWithTag(2)?.layer.cornerRadius = 90
+        traficLight.viewWithTag(3)?.layer.cornerRadius = 90
+    
+        
     }
 
 
